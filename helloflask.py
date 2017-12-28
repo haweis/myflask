5591fedc9728dec5aad96c85c641e9f4f14b8374
zhouxiaoyan <zhouxiaoyan@zhouxiaoyandeMacBook-Pro.local>
2017-12-28 23:41:00 +0800

    practice login and post

--- helloflask.py
+++ helloflask.py
@@ -1,24 +1,36 @@
-from flask import Flask
+#coding:utf-8 
+
+from flask import Flask, session, redirect, url_for, escape, request
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
 
-@app.route('/hello')
-def hello():
-    return 'Hello World'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if __name__ == '__main__':
     app.debug = True
